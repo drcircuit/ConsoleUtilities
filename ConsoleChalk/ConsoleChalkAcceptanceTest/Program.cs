8f412107010cd4a3a4a026a69414d19aecf1df24
espensl <espensl@compello.com>
2016-09-01 17:35:23 +0200

    made acceptance tests easier to read.

--- ConsoleChalk/ConsoleChalkAcceptanceTest/Program.cs
+++ ConsoleChalk/ConsoleChalkAcceptanceTest/Program.cs
@@ -9,15 +9,24 @@ namespace ConsoleChalkAcceptanceTest
         private static void Main(string[] args)
         {
             Console.WriteLine("This text should be normal");
+            Console.WriteLine();
             Console.WriteLine("Green".Green() + " should appear green");
+            Console.WriteLine();
             Console.WriteLine("Blue".Blue(true) + "should appear blue on it's own line");
+            Console.WriteLine();
             Console.WriteLine($"{"Red".Red()} Should appear red, even though it's defined in an expression");
+            Console.WriteLine();
             Console.WriteLine(" Writing " + "red".Red() + " in the middle of a string, sadly moves it first. Color calls are resolved first.");
-            Console.WriteLine("Yellow".Yellow() + " and ".Gray() + "Red".Red() + " should look like two different colors...");
+            Console.WriteLine();
             Console.WriteLine("However writing ".Gray() + "red".Red() + " in the middle of a chain of calls, works!".Gray());
+            Console.WriteLine();
+            Console.WriteLine("Yellow".Yellow() + " and ".Gray() + "Red".Red() + " should look like two different colors...");
+            Console.WriteLine();
             "Call red on a string, will render it in red".Red();
-
-            Console.Read();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press (almost) any key for some random color strangeness.".Gray() + "(not -3 ...)".Green());
+            Console.ReadKey();
             var words = new List<string>
             {
                 "zoom",
@@ -36,6 +45,9 @@ namespace ConsoleChalkAcceptanceTest
                 "grace",
                 "hopper",
                 "mandelbrot",
+                "feynman",
+                "hilbert",
+                "hardy",
                 "ramanujan",
                 "infinity",
                 "undefined",
@@ -45,7 +57,7 @@ namespace ConsoleChalkAcceptanceTest
                 "carbonara",
                 "chicks4free"
             };
-            
+
             var colors = new List<Func<string, string>>
             {
                 ConsoleChalk.Gray,
@@ -58,14 +70,17 @@ namespace ConsoleChalkAcceptanceTest
                 ConsoleChalk.Magenta
             };
             PrintRandomWordsInRandomColors(words, colors);
+            Console.WriteLine();
+            Console.WriteLine();
+            "Press (almost) any key to exit".Magenta(true);
             Console.ReadKey();
         }
 
         private static void PrintRandomWordsInRandomColors(List<string> words, List<Func<string, string>> colors)
         {
-            Random r = new Random(1337);
-            for (int i = 1000; i > 0; i--)
-                colors[r.Next(0, colors.Count - 1)](words[r.Next(0, words.Count - 1)]);
+            var r = new Random(1337);
+            for (var i = 1000; i > 0; i--)
+                colors[r.Next(0, colors.Count - 1)](words[r.Next(0, words.Count - 1)].ToUpper());
         }
     }
 }
