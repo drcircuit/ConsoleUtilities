add18021770e501e156d5a115e4a59cc5b16d4f2
espensl <espensl@compello.com>
2016-09-01 17:03:01 +0200

    added Console Chalk solution

---
+++ ConsoleChalk/ConsoleChalk/ConsoleChalk.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrCircuit.ConsoleUtilities
+{
+    public static class ConsoleChalk
+    {
+        private static readonly Dictionary<string, ConsoleColor> Palette = new Dictionary<string, ConsoleColor>
+        {
+            {"red", ConsoleColor.Red},
+            {"green", ConsoleColor.Green},
+            {"yellow", ConsoleColor.Yellow},
+            {"cyan", ConsoleColor.Cyan},
+            {"blue", ConsoleColor.Blue},
+            {"magenta", ConsoleColor.Magenta},
+            {"gray", ConsoleColor.Gray},
+            {"white", ConsoleColor.White}
+        };
+
+        public static string Blue(this string s)
+        {
+            return Blue(s, false);
+        }
+
+        public static string Blue(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["blue"]);
+            else
+                ColoredWrite(s, Palette["blue"]);
+            return string.Empty;
+        }
+
+        public static string Cyan(this string s)
+        {
+            return Cyan(s, false);
+        }
+
+        public static string Cyan(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["cyan"]);
+            else
+                ColoredWrite(s, Palette["cyan"]);
+            return string.Empty;
+        }
+
+        public static string Gray(this string s)
+        {
+            return Gray(s, false);
+        }
+
+        public static string Gray(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["gray"]);
+            else
+                ColoredWrite(s, Palette["gray"]);
+            return string.Empty;
+        }
+
+        public static string Green(this string s)
+        {
+            return Green(s, false);
+        }
+
+        public static string Green(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["green"]);
+            else
+                ColoredWrite(s, Palette["green"]);
+            return string.Empty;
+        }
+
+        public static string Magenta(this string s)
+        {
+            return Magenta(s, false);
+        }
+
+        public static string Magenta(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["magenta"]);
+            else
+                ColoredWrite(s, Palette["magenta"]);
+            return string.Empty;
+        }
+
+        public static string Red(this string s)
+        {
+            return Red(s, false);
+        }
+
+        public static string Red(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["red"]);
+            else
+                ColoredWrite(s, Palette["red"]);
+            return string.Empty;
+        }
+
+        public static string White(this string s)
+        {
+            return White(s, false);
+        }
+
+        public static string White(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["white"]);
+            else
+                ColoredWrite(s, Palette["white"]);
+            return string.Empty;
+        }
+
+        public static string Yellow(this string s)
+        {
+            return Yellow(s, false);
+        }
+
+        public static string Yellow(this string s, bool writeLine)
+        {
+            if (writeLine)
+                ColoredWriteLine(s, Palette["yellow"]);
+            else
+                ColoredWrite(s, Palette["yellow"]);
+            return string.Empty;
+        }
+
+        private static void ColoredWrite(string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+
+        private static void ColoredWriteLine(string s, ConsoleColor color)
+        {
+            ColoredWrite($"{s}{Environment.NewLine}", color);
+        }
+    }
+}
---
+++ ConsoleChalk/ConsoleChalkAcceptanceTest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using DrCircuit.ConsoleUtilities;
+
+namespace ConsoleChalkAcceptanceTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This text should be normal");
+            Console.WriteLine("Green".Green() + " should appear green");
+            Console.WriteLine("Blue".Blue(true) + "should appear blue on it's own line");
+            Console.WriteLine($"{"Red".Red()} Should appear red, even though it's defined in an expression");
+            Console.WriteLine(" Writing " + "red".Red() + " in the middle of a string, sadly moves it first. Color calls are resolved first.");
+            Console.WriteLine("Yellow".Yellow() + " and ".Gray() + "Red".Red() + " should look like two different colors...");
+            Console.WriteLine("However writing ".Gray() + "red".Red() + " in the middle of a chain of calls, works!".Gray());
+            "Call red on a string, will render it in red".Red();
+
+            Console.Read();
+            var words = new List<string>
+            {
+                "zoom",
+                "fractal",
+                "zeta",
+                "riemann",
+                "complex",
+                "imaginary",
+                "quantum",
+                "einstein",
+                "bohr",
+                "csharp",
+                "cplusplus",
+                "basic",
+                "cobol",
+                "grace",
+                "hopper",
+                "mandelbrot",
+                "ramanujan",
+                "infinity",
+                "undefined",
+                "stackoverflow",
+                "bolle",
+                "danish",
+                "carbonara",
+                "chicks4free"
+            };
+            
+            var colors = new List<Func<string, string>>
+            {
+                ConsoleChalk.Gray,
+                ConsoleChalk.Green,
+                ConsoleChalk.Blue,
+                ConsoleChalk.Cyan,
+                ConsoleChalk.Red,
+                ConsoleChalk.White,
+                ConsoleChalk.Yellow,
+                ConsoleChalk.Magenta
+            };
+            PrintRandomWordsInRandomColors(words, colors);
+            Console.ReadKey();
+        }
+
+        private static void PrintRandomWordsInRandomColors(List<string> words, List<Func<string, string>> colors)
+        {
+            Random r = new Random(1337);
+            for (int i = 1000; i > 0; i--)
+                colors[r.Next(0, colors.Count - 1)](words[r.Next(0, words.Count - 1)]);
+        }
+    }
+}
